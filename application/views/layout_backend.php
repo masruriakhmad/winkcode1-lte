f63b6fcfb76140f04430565cce9b13aea1d55062
Akhmad Masruri <masruriakhmad@gmail.com>
2022-11-03 09:41:39 +0700

    V.2022.11.03.09.41

--- application/views/layout_backend.php
+++ application/views/layout_backend.php
@@ -90,9 +90,9 @@ $CI->load->model('Users_model');
 
 
         <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
+        <!-- <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-        </div>
+        </div> -->
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
